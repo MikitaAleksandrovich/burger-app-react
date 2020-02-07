d832fd7a0afc8560d1ddd041e316bd44e484660d
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-02-07 21:34:11 +0300

    Display ingredients dinamicly by mapping them

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -6,11 +6,20 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 
 const burger = (props) => {
+
+    const transformedIngredients = Object.keys(props.ingredients).
+        map((ingKey) => {
+            return [...Array(props.ingredients[ingKey])].map((_, i) => {
+                return  <BurgerIngredient key={ingKey + i} type={ingKey} />
+            })
+        });
+
+        console.log(transformedIngredients);
+
     return (
         <div className="burger">
             <BurgerIngredient type='bread-top'/>
-            <BurgerIngredient type='cheese'/>
-            <BurgerIngredient type='meat'/>
+            {transformedIngredients}
             <BurgerIngredient type='bread-bottom'/>
         </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,11 +5,22 @@ import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
 
+    state = {
+        ingredients: {
+            salad: 1,
+            bacon: 1,
+            cheese: 2,
+            meat: 2,
+        }
+    }
 
     render() {
+
+        const { ingredients } = this.state;
+
         return(
             <Aux>
-                <Burger />
+                <Burger ingredients={ingredients} />
                 <div>
                     Build Controls
                 </div>
