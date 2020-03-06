581f6360bc57c07c2196ce38e189368ee49181d8
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-03-06 11:43:47 +0300

    Updates

---
+++ precache-manifest.319884dd8c876359e3cde2561e5f0b3b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2fe33db910d0787f9b4a62f1c53559c4",
+    "url": "/burger-app-react/index.html"
+  },
+  {
+    "revision": "1cdeeb0584696441b015",
+    "url": "/burger-app-react/static/css/main.5ee0fd95.chunk.css"
+  },
+  {
+    "revision": "c3c0cb7a7942a324bfe9",
+    "url": "/burger-app-react/static/js/2.b04a6420.chunk.js"
+  },
+  {
+    "revision": "1cdeeb0584696441b015",
+    "url": "/burger-app-react/static/js/main.5acbe54c.chunk.js"
+  },
+  {
+    "revision": "1e56a88d654e1bd1b673",
+    "url": "/burger-app-react/static/js/runtime-main.23fad333.js"
+  }
+]);
