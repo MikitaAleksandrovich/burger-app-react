5ead866fb6d33e2a0b10ed4d36aafea269a9135d
Mikita Aleksandrovich <45469739+MikitaAleksandrovich@users.noreply.github.com>
2020-04-16 20:03:12 +0300

    Fix router's bug

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
-import React, { Component } from "react";
-import { Route, Switch } from 'react-router-dom';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
 
-class App extends Component {
 
-  render() {
-    return (
-      <div>
-        <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
-        </Layout>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Layout>
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/" exact component={BurgerBuilder} />
+        </Switch>
+      </Layout>
+    </div>
+  </Router>
+);
 
 
 export default App;
